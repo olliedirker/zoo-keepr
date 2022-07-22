eda9404618b4fe8d256d5b06c8b5767ab5c49025
olliedirker <olliedirker@gmail.com>
2022-07-21 20:43:54 -0500

    added 404 return, and paths for app

--- server.js
+++ server.js
@@ -42,12 +42,26 @@ if (query. personalityTraits){
     return filteredResults;
 }
 
+function findById(id, animalsArray){
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query){
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    if (result){
+    res.json(result);
+    }else{
+        res.send(404);
+    }
 });
 
 //end of server.js

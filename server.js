08997cc09361ffe796c1b28d6a35ce9f3d4d3f1e
olliedirker <olliedirker@gmail.com>
2022-07-23 13:13:45 -0500

    began insomnia usage

--- server.js
+++ server.js
@@ -2,66 +2,76 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parse incoming string or array data 
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data 
+app.use(express.json());
 
-function filterByQuery(query, animalsArray){
+function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     //Note that we save the animalsArray as filteredResults here :
     let filteredResults = animalsArray;
-if (query. personalityTraits){
-    //save personalityTraits as a dedicated arrau
-    //if personalityTraits is a string, place it into a new array and save
-    if(typeof query.personalityTraits === 'string'){
-        personalityTraitsArray = [query.personalityTraits];
-    }else{
-        personalityTraitsArray = query.personalityTraits;
+    if (query.personalityTraits) {
+        //save personalityTraits as a dedicated arrau
+        //if personalityTraits is a string, place it into a new array and save
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        //loop through each trait in the personalityTraits array;
+        personalityTraitsArray.forEach(trait => {
+            //check the trait against each animal in the filteredResults array.
+            //Remember it is initaially a copy of the animalsArray,
+            //but here we're updating it for each trait in the .forEach()loop.
+            //For each trait being targeted by the filter, the filteredResults
+            //array will then contain only the entries that contain the trait,
+            //so at the end we'll have an array of animals that have every one
+            //of the traits when the .forEach() loop is finished.
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
     }
-    //loop through each trait in the personalityTraits array;
-    personalityTraitsArray.forEach(trait => {
-        //check the trait against each animal in the filteredResults array.
-        //Remember it is initaially a copy of the animalsArray,
-        //but here we're updating it for each trait in the .forEach()loop.
-        //For each trait being targeted by the filter, the filteredResults
-        //array will then contain only the entries that contain the trait,
-        //so at the end we'll have an array of animals that have every one
-        //of the traits when the .forEach() loop is finished.
-        filteredResults = filteredResults.filter(
-            animal => animal.personalityTraits.indexOf(trait) !== -1
-        );
-    });
-}
 
-    if(query.diet){
+    if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
-    if(query.species){
-        filteredResults = filteredResults.filter(animal=> animal.species === query.species);
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
     }
-    if(query.name){
-        filteredResults = filteredResults.filter(animal=> animal.name === query.name);
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
 }
 
-function findById(id, animalsArray){
+function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
-    if (req.query){
+    if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
 });
 
-app.get('/api/animals/:id', (req,res) => {
+app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
-    if (result){
-    res.json(result);
-    }else{
+    if (result) {
+        res.json(result);
+    } else {
         res.send(404);
     }
+});
+
+app.post('/api/animals', (req, res) => {
+    //req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
 });
 
 //end of server.js
